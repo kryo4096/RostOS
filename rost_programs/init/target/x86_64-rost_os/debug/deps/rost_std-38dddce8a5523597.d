5bf585768a9fd9314dd97022d2421d5b21cea240
kryo4096 <kryo4096@gmail.com>
2018-12-16 23:11:04 +0100

    fixed .gitignore

--- rost_programs/init/target/x86_64-rost_os/debug/deps/rost_std-38dddce8a5523597.d
+++
@@ -1,5 +0,0 @@
-/home/jonas/Projects/Rust/RostOS/rost_programs/init/target/x86_64-rost_os/debug/deps/librost_std-38dddce8a5523597.rlib: /home/jonas/Projects/Rust/RostOS/rost_std/src/lib.rs
-
-/home/jonas/Projects/Rust/RostOS/rost_programs/init/target/x86_64-rost_os/debug/deps/rost_std-38dddce8a5523597.d: /home/jonas/Projects/Rust/RostOS/rost_std/src/lib.rs
-
-/home/jonas/Projects/Rust/RostOS/rost_std/src/lib.rs:
--- rost_programs/init/target/x86_64-rost_os/debug/init.d
+++
@@ -1,1 +0,0 @@
-/home/jonas/Projects/Rust/RostOS/rost_programs/init/target/x86_64-rost_os/debug/init: /home/jonas/Projects/Rust/RostOS/rost_programs/init/src/main.rs /home/jonas/Projects/Rust/RostOS/rost_std/src/lib.rs
