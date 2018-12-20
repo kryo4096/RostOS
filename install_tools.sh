344e674ed34bb0b1b14c4934b423fff14c74ba4d
kryo4096 <kryo4096@users.noreply.github.com>
2018-12-20 18:38:41 +0100

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -4,6 +4,7 @@ cargo install --path rost_fs/fscreate
 cargo install bootimage
 cargo install cargo-xbuild
 
+rustup toolchain install nightly
 rustup override add nightly
 rustup component add rust-src
 
